f0f89d05869e75f942cd9189d923a84651921431
Smorkster <smorkster@gmail.com>
2016-05-27 01:35:34 +0200

    Version 1.0
    Initial release
    Added all IPStatus messages from MSDN

---
+++ MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace PingSnurra
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox txtAddress;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnStopPing;
+		private System.Windows.Forms.Button btnStartPing;
+		private System.Windows.Forms.Button btnSaveLog;
+		private System.Windows.Forms.TextBox txtPingLog;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtAddress = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnStopPing = new System.Windows.Forms.Button();
+			this.btnStartPing = new System.Windows.Forms.Button();
+			this.btnSaveLog = new System.Windows.Forms.Button();
+			this.txtPingLog = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// txtAddress
+			// 
+			this.txtAddress.Location = new System.Drawing.Point(1, 28);
+			this.txtAddress.Name = "txtAddress";
+			this.txtAddress.Size = new System.Drawing.Size(448, 20);
+			this.txtAddress.TabIndex = 0;
+			this.txtAddress.Text = "google.com";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(1, 4);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(80, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Address to ping";
+			// 
+			// btnStopPing
+			// 
+			this.btnStopPing.Location = new System.Drawing.Point(168, 0);
+			this.btnStopPing.Name = "btnStopPing";
+			this.btnStopPing.Size = new System.Drawing.Size(75, 23);
+			this.btnStopPing.TabIndex = 2;
+			this.btnStopPing.Text = "Stop";
+			this.btnStopPing.UseVisualStyleBackColor = true;
+			this.btnStopPing.Click += new System.EventHandler(this.BtnStopPingClick);
+			// 
+			// btnStartPing
+			// 
+			this.btnStartPing.Location = new System.Drawing.Point(87, 0);
+			this.btnStartPing.Name = "btnStartPing";
+			this.btnStartPing.Size = new System.Drawing.Size(75, 23);
+			this.btnStartPing.TabIndex = 3;
+			this.btnStartPing.Text = "Start pinging";
+			this.btnStartPing.UseVisualStyleBackColor = true;
+			this.btnStartPing.Click += new System.EventHandler(this.BtnStartPingClick);
+			// 
+			// btnSaveLog
+			// 
+			this.btnSaveLog.Location = new System.Drawing.Point(374, 0);
+			this.btnSaveLog.Name = "btnSaveLog";
+			this.btnSaveLog.Size = new System.Drawing.Size(75, 23);
+			this.btnSaveLog.TabIndex = 5;
+			this.btnSaveLog.Text = "Save log";
+			this.btnSaveLog.UseVisualStyleBackColor = true;
+			this.btnSaveLog.Click += new System.EventHandler(this.BtnSaveLogClick);
+			// 
+			// txtPingLog
+			// 
+			this.txtPingLog.Location = new System.Drawing.Point(1, 54);
+			this.txtPingLog.Multiline = true;
+			this.txtPingLog.Name = "txtPingLog";
+			this.txtPingLog.Size = new System.Drawing.Size(448, 559);
+			this.txtPingLog.TabIndex = 6;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(448, 613);
+			this.Controls.Add(this.txtPingLog);
+			this.Controls.Add(this.btnSaveLog);
+			this.Controls.Add(this.btnStartPing);
+			this.Controls.Add(this.btnStopPing);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtAddress);
+			this.Name = "MainForm";
+			this.Text = "PingSnurra";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace PingSnurra
+{
+	public partial class MainForm : Form
+	{
+		Ping pinger = new Ping();
+		PingReply prep;
+		Timer t;
+		List<string[]> badpackets = new List<string[]>();
+		public MainForm()
+		{
+			InitializeComponent();
+			t = new Timer();
+			t.Interval = 1000;
+			t.Tick += TimerCallback;
+			Text = "PingSnurra version " + System.Reflection.Assembly.GetExecutingAssembly()
+				.GetName().Version;
+		}
+
+		void TimerCallback (object sender, EventArgs e)
+		{
+			prep = pinger.Send(txtAddress.Text);
+			switch (prep.Status)
+			{
+				case IPStatus.TimedOut:
+					txtPingLog.AppendText("Pingrequest timed out. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.Success:
+					txtPingLog.AppendText("Roundtriptime: " + prep.RoundtripTime + " ms" + "\r\n");
+					break;
+				case IPStatus.TtlExpired:
+				case IPStatus.TimeExceeded:
+					txtPingLog.AppendText("The ICMP echo request failed because its Time to Live (TTL) value reached zero, causing the forwarding node (router or gateway) to discard the packet. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.DestinationHostUnreachable:
+					txtPingLog.AppendText("The ICMP echo request failed because the destination computer is not reachable. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.BadDestination:
+					txtPingLog.AppendText("The ICMP echo request failed because the destination IP address cannot receive ICMP echo requests or should never appear in the destination address field of any IP datagram. For example, calling Send and specifying IP address '000.0.0.0' returns this status. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.BadHeader:
+					txtPingLog.AppendText("The ICMP echo request failed because the header is invalid. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.BadOption:
+					txtPingLog.AppendText("The ICMP echo request failed because it contains an invalid option. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.BadRoute:
+					txtPingLog.AppendText("The ICMP echo request failed because there is no valid route between the source and destination computers. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.DestinationNetworkUnreachable:
+					txtPingLog.AppendText("The ICMP echo request failed because the network that contains the destination computer is not reachable. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.DestinationPortUnreachable:
+					txtPingLog.AppendText("The ICMP echo request failed because the port on the destination computer is not available. (time " + DateTime.Now + ")\r\n");
+					break;
+				case (IPStatus) 11004:
+					var host = Dns.GetHostEntry(txtAddress.Text);
+					var ip = host.AddressList[host.AddressList.Length - 1];
+					if (ip.AddressFamily == AddressFamily.InterNetwork)
+						txtPingLog.AppendText("The ICMP echo request failed because the destination computer that is specified in an ICMP echo message is not reachable, because it does not support the packet's protocol. This value applies only to IPv4. This value is described in IETF RFC 1812 as Communication Administratively Prohibited. (time " + DateTime.Now + ")\r\n");
+					else
+						txtPingLog.AppendText("The ICMPv6 echo request failed because contact with the destination computer is administratively prohibited. This value applies only to IPv6. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.DestinationScopeMismatch:
+					txtPingLog.AppendText("The ICMP echo request failed because the source address and destination address that are specified in an ICMP echo message are not in the same scope. This is typically caused by a router forwarding a packet using an interface that is outside the scope of the source address. Address scopes (link-local, site-local, and global scope) determine where on the network an address is valid. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.DestinationUnreachable:
+					txtPingLog.AppendText("The ICMP echo request failed because the destination computer that is specified in an ICMP echo message is not reachable; the exact cause of problem is unknown. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.HardwareError:
+					txtPingLog.AppendText("The ICMP echo request failed because of a hardware error. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.IcmpError:
+					txtPingLog.AppendText("The ICMP echo request failed because of an ICMP protocol error. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.NoResources:
+					txtPingLog.AppendText("The ICMP echo request failed because of insufficient network resources. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.PacketTooBig:
+					txtPingLog.AppendText("The ICMP echo request failed because the packet containing the request is larger than the maximum transmission unit (MTU) of a node (router or gateway) located between the source and destination. The MTU defines the maximum size of a transmittable packet. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.ParameterProblem:
+					txtPingLog.AppendText("The ICMP echo request failed because a node (router or gateway) encountered problems while processing the packet header. This is the status if, for example, the header contains invalid field data or an unrecognized option. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.SourceQuench:
+					txtPingLog.AppendText("The ICMP echo request failed because the packet was discarded. This occurs when the source computer's output queue has insufficient storage space, or when packets arrive at the destination too quickly to be processed. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.TtlReassemblyTimeExceeded:
+					txtPingLog.AppendText("The ICMP echo request failed because the packet was divided into fragments for transmission and all of the fragments were not received within the time allotted for reassembly. RFC 2460 (available at www.ietf.org) specifies 60 seconds as the time limit within which all packet fragments must be received. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.Unknown:
+					txtPingLog.AppendText("The ICMP echo request failed for an unknown reason. (time " + DateTime.Now + ")\r\n");
+					break;
+				case IPStatus.UnrecognizedNextHeader:
+					txtPingLog.AppendText("The ICMP echo request failed because the Next Header field does not contain a recognized value. The Next Header field indicates the extension header type (if present) or the protocol above the IP layer, for example, TCP or UDP. (time " + DateTime.Now + ")\r\n");
+					break;
+			}
+		}
+		void BtnStopPingClick (object sender, EventArgs e)
+		{
+			t.Stop();
+			txtPingLog.AppendText("Pingloop stopped by user\r\n");
+		}
+		void BtnSaveLogClick (object sender, EventArgs e)
+		{
+			File.WriteAllText(@"H:\Pinglog.txt", txtPingLog.Text);
+			MessageBox.Show(@"H:\Pinglog.txt saved", "");
+		}
+		void BtnStartPingClick (object sender, EventArgs e)
+		{
+			IPAddress adress;
+			IPHostEntry hostInfo = Dns.GetHostEntry(txtAddress.Text);
+
+
+			if (IPAddress.TryParse(txtAddress.Text, out adress))
+			{
+				txtPingLog.AppendText("Starts pinging " + txtAddress.Text + " (starttime " + DateTime.Now + ")\r\n");
+				t.Start();
+			} else
+			{
+				txtPingLog.AppendText("Starts pinging " + txtAddress.Text + " at IP: " + hostInfo.AddressList[hostInfo.AddressList.Length - 1] + " (starttime " + DateTime.Now + ")\r\n");
+				t.Start();
+			}
+		}
+	}
+}
